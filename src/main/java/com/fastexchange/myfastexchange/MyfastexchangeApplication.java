b4a72eb7e7709573057b8fbef7fa359c6e7eb251
bmbmbm9 <bmbmbm9@protonmail.com>
2023-07-07 15:21:11 +0300

    moved to mysql

--- src/main/java/com/fastexchange/myfastexchange/MyfastexchangeApplication.java
+++ src/main/java/com/fastexchange/myfastexchange/MyfastexchangeApplication.java
@@ -2,7 +2,9 @@ package com.fastexchange.myfastexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyfastexchangeApplication {
 

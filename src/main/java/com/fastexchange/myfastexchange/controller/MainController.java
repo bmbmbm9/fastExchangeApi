550d25d15bb33217d2ef83da485bf90150e5a002
bmbmbm9 <bmbmbm9@protonmail.com>
2023-07-08 14:21:36 +0300

    added cross origin

--- src/main/java/com/fastexchange/myfastexchange/controller/MainController.java
+++ src/main/java/com/fastexchange/myfastexchange/controller/MainController.java
@@ -5,13 +5,10 @@ import com.fastexchange.myfastexchange.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
+@CrossOrigin(origins = "https://fastexchange365.com")
 @RestController
 @RequestMapping("/api")
 public class MainController {
